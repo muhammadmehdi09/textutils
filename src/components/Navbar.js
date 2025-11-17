751f20ea287e5862b55bbfe1fe9db35e72ba7280
Coding Verse <muhammadmehdi09@gmail.com>
2025-11-17 08:42:24 +0500

    Updating logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,7 @@ export default function Navbar(props) {
             <div className="flex space-x-4 my-auto justify-between px-4">
                 <div className="right flex">
                     {/* <a className="m-2 ml-0 font-semibold text-xl" href="/">{props.title}</a> */}
-                    <img src={logo} alt="TextUtils" className="w-32 h-6" />
+                    <img src={logo} alt="TextUtils" className="w-[120px] h-8" />
                 </div>
                 <div className="left my-auto">
                     <label className="relative inline-flex items-center cursor-pointer mt-1">
